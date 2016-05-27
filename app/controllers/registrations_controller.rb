c4aae2694bb50acf0336e925cc33acae3cfcd2f4
JLikesZzz <jeremy_lzz@hotmail.com>
2016-05-27 15:59:48 +0800

    fixed something again

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,40 +1,40 @@
 class RegistrationsController < Devise::RegistrationsController
 
-	# def create
-	#
-  #   build_resource(sign_up_params)
-	#
-  #   resource.save
-  #   yield resource if block_given?
-  #   if resource.persisted?
-	#
-  #     if resource.active_for_authentication?
-  #       set_flash_message! :notice, :signed_up
-  #       sign_up(resource_name, resource)
-  #       respond_with resource, location: after_sign_up_path_for(resource)
-  #     else
-  #       set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-  #       expire_data_after_sign_in!
-  #       respond_with resource, location: after_inactive_sign_up_path_for(resource)
-  #     end
-  #   else
-  #     clean_up_passwords resource
-  #     set_minimum_password_length
-  #     respond_with resource
-  #   end
-  # end
+	def create
+
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
 
 	#
-  # protected
-  # def update_resource(resource, params)
-  #   resource.update_without_password(params)
-  # end
+  protected
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 	#
   def sign_up_params
     params.require(:user).permit(:fullname, :email, :password, :password_confirmation, :code)
   end
 	#
-  # def account_update_params
-  #   params.require(:user).permit(:avatar, :fullname, :phone_number, :description, :interest, :email, :password, :password_confirmation, :code, :image => [])
-  # end
+  def account_update_params
+    params.require(:user).permit(:avatar, :fullname, :phone_number, :description, :interest, :email, :password, :password_confirmation, :code, :image => [])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
     devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
     devise :omniauthable, :omniauth_providers => [:facebook]
     validates :fullname, presence: true, length: {maximum: 50}
 
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
     has_many :photos
     has_many :transactions
 
-    # before_create :confirmation_token
+    before_create :confirmation_token
 
     has_attached_file :avatar, styles: { medium: "300x300>", thumb: "50x50#", profile: '200x200>' }, default_url: nil
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
