7355813aec71b6189dd8fe6b537eae2854fe820e
kevin118 <kevin.limym@gmail.com>
2016-05-28 09:15:55 +0800

    Added premium

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:user_id])
     @user.update(premium_params)
-    redirect_to @user
+    @user.premium = true
+    redirect_to product_path(1)
   end
 
 
