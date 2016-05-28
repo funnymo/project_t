6f27b426dcfc91776fbe3facae5ea8fc78eec97a
Morgan <mky.wee@gmail.com>
2016-05-28 16:04:22 +0800

    factoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,15 @@
 class UsersController < ApplicationController
-
   def show
-    @user = User.find_by(code: params[:code]) || User.find(params[:id])
-    if @user.nil?
-      redirect_to "/"
-    end
+    @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:user_id])
-    @user.update(premium_params)
-    @user.premium = true
-    redirect_to product_path(1)
+    redirect_to @user
   end
 
-
   private
-
-# Use strong_parameters for attribute whitelisting
-# Be sure to update your create() and update() controller methods.
-
-
   def premium_params
     params.require(:user).permit(:code)
   end
-
 end
