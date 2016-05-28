2c79259d31b59d7f9ab30beb2963b2a3728f22bc
Morgan <mky.wee@gmail.com>
2016-05-28 22:54:29 +0800

    fixed user search redirection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,26 @@
 class UsersController < ApplicationController
+  # def show
+  #   @user = User.find(params[:id])
+  #   if params[:code]
+  #     @user = User.find_by(code: params[:code])
+  #   elsif @user.id != current_user.id
+  #     redirect_to root_path, alert: "You don't have permission to do that."
+  #   else
+
+  #   end
+  # end
+  #
   def show
-    @user = User.find(params[:id])
+    if params[:code].nil?
+      user = User.find(params[:id])
+      if current_user == user
+        @user = User.find(current_user.id)
+      else
+        redirect_to root_path, alert: "You don't have permission to do that"
+      end
+    else
+      @user = User.find_by(code: params[:code])
+    end
   end
 
   def update
