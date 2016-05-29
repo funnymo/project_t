c8a23c7c147bd8822b7218524072f2240d590136
kevin118 <kevin.limym@gmail.com>
2016-05-29 15:21:11 +0800

    FB Test 2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do 
 
 
-provider :facebook, 'YOUR_APP_ID', 'YOUR_SECRET_KEY', 
+provider :facebook, '1309760222384824', 'YOUR_SECRET_KEY', 
            {:scope => 'PERMISSION_1, PERMISSION_2, PERMISSION_3...', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 
        end
