1dcea9bd9539024f62d5910d12ffb927e31a9608
JLikesZzz <jeremy_lzz@hotmail.com>
2016-05-29 16:36:53 +0800

    tried user controller failed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    if current_user.id == params[:id] && params[:code].nil?
+      redirect_to root_path, alert: "You don't have permission to do that"
+    elsif params[:code].nil? && current_user.id == params[:id]
+      @user = User.find_by(idcurrent_user.id)
+    elsif params[:code]
+      @user = User.find_by(code: params[:code])
+    end
   end
 
   def update
